2b13cd54e697a18cb57cde726b781c45dfccbb04
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-09-21 16:32:45 +0300

    main.c

--- main.c
+++ main.c
@@ -8,12 +8,12 @@ bus_t bus = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
-	char *content;
+	char *_content;
 	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	unsigned int _counter = 0;
 
 	if (argc != 2)
 	{
@@ -29,15 +29,15 @@ int main(int argc, char *argv[])
 	}
 	while (read_line > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
+		_content = NULL;
+		read_line = getline(&_content, &size, file);
+		bus._content = _content;
+		_counter++;
 		if (read_line > 0)
 		{
-			execute(content, &stack, counter, file);
+			execute(_content, &stack, _counter, file);
 		}
-		free(content);
+		free(_content);
 	}
 	free_stack(stack);
 	fclose(file);
