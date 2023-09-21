99ac3f1215c685db0e4da4c25e54151ba7b65628
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-09-21 16:23:36 +0300

    _stack.c

--- _stack.c
+++ _stack.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 /**
- * f_stack - prints the top
- * @head: stack head
- * @counter: line_number
+ * f_stack - _prints the top
+ * @head: _stack head
+ * @counter: _line_number
  * Return: no return
 */
 void f_stack(stack_t **head, unsigned int counter)
